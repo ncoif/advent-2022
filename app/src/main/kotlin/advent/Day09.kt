44c7b4614831c832a7c8c446a92b328eb16960ac
ncoif <ncoif@gmx.com>
2022-12-16 08:56:28 +0000

    Day09 wip move tail

--- app/src/main/kotlin/advent/Day09.kt
+++ app/src/main/kotlin/advent/Day09.kt
@@ -23,13 +23,88 @@ class Day09(private val input: URL) {
         return Move(direction, split[1].toInt())
     }
 
-    fun parseMoves(lines: List<String>) : List<Move> {
-        return lines.map { extractMove(it) }
+    data class Position(val x: Int, val y: Int) {
+        fun applyDirection(direction: Direction) : Position {
+            return when(direction) {
+                Direction.LEFT -> Position(x - 1, y)
+                Direction.RIGHT -> Position(x + 1, y)
+                Direction.UP -> Position(x, y + 1)
+                Direction.DOWN -> Position(x, y - 1)
+            }
+        }
+
+        fun isNeighbour(other: Position) : Boolean{
+            return when(other) {
+                Position(x, y) -> true // same
+                Position(x + 1, y) -> true // right
+                Position(x + 1, y + 1) -> true // top right
+                Position(x, y + 1) -> true // top
+                Position(x - 1, y + 1) -> true // top left
+                Position(x - 1, y) -> true // left
+                Position(x - 1, y - 1) -> true // bottom left
+                Position(x, y - 1) -> true // bottom
+                Position(x + 1, y - 1) -> true // bottom right
+                else -> false
+            }
+        }
+
+        fun allNeighbour(): List<Position> {
+            return listOf(
+                Position(x + 1, y), // right
+                Position(x, y + 1), // top
+                Position(x - 1, y), // left
+                Position(x, y - 1), // bottom
+                Position(x + 1, y + 1), // top right
+                Position(x - 1, y + 1), // top left
+                Position(x - 1, y - 1), // bottom left
+                Position(x + 1, y - 1), // bottom right
+            )
+        }
+    }
+
+    data class State(var headPosition : Position, var tailPosition: Position, val pastPositions : MutableList<Position>) {
+        fun playMove(move: Move) {
+            (0 until move.distance).forEach{ _ ->
+                playDirection(move.direction)
+                moveTail()
+            }
+        }
+
+        private fun playDirection(direction: Direction) {
+            val newHeadPosition = headPosition.applyDirection(direction)
+            pastPositions.add(newHeadPosition)
+            headPosition = newHeadPosition
+        }
+
+        private fun moveTail() {
+            if (!tailPosition.isNeighbour(headPosition)) {
+                val newTailPosition = findNewTailPosition()
+                pastPositions.add(newTailPosition)
+                tailPosition = newTailPosition
+            }
+        }
+
+        private fun findNewTailPosition() : Position {
+            // try all neighbour position until one is valid
+            for (neighbour in tailPosition.allNeighbour()) {
+                if (neighbour.isNeighbour(headPosition)) {
+                    return neighbour
+                }
+            }
+            throw IllegalArgumentException("No valid tail position found")
+        }
+    }
+
+    fun initialState() : State {
+        return State(Position(0, 0), Position(0, 0), mutableListOf(Position(0, 0)))
     }
 
     fun solvePart1(): Int {
         val input = File(input.toURI()).readLines()
-        println(parseMoves(input))
+        val moves = input.map { extractMove(it) }
+
+        val state = initialState()
+        moves.forEach { state.playMove(it) }
 
         return 0
     }
--- app/src/test/kotlin/advent/Day09Test.kt
+++ app/src/test/kotlin/advent/Day09Test.kt
@@ -1,5 +1,6 @@
 package advent
 
+import advent.Day09.Position
 import org.junit.Test
 import java.io.File
 import kotlin.test.assertEquals
@@ -9,6 +10,46 @@ internal class Day09Test {
     private fun underTest() : Day09 {
         val testInput = this.javaClass.getResource("/day09-test-input.txt")!!
         return Day09(testInput)
+    }
+
+    @Test
+    fun testMoveTailHorizontally() {
+        val state = Day09.State(Position(2, 1), Position(1, 1), mutableListOf(Position(2, 1), Position(1, 1)))
+
+        state.playMove(Day09.Move(Day09.Direction.RIGHT, 1))
+
+        assertEquals(Position(3, 1), state.headPosition)
+        assertEquals(Position(2, 1), state.tailPosition)
+    }
+
+    @Test
+    fun testMoveTailVertically() {
+        val state = Day09.State(Position(1, 2), Position(1, 3), mutableListOf(Position(1, 2), Position(1, 3)))
+
+        state.playMove(Day09.Move(Day09.Direction.DOWN, 1))
+
+        assertEquals(Position(1, 1), state.headPosition)
+        assertEquals(Position(1, 2), state.tailPosition)
+    }
+
+    @Test
+    fun testMoveTailDiagonally1() {
+        val state = Day09.State(Position(2, 2), Position(1, 1), mutableListOf(Position(2, 2), Position(1, 1)))
+
+        state.playMove(Day09.Move(Day09.Direction.UP, 1))
+
+        assertEquals(Position(2, 3), state.headPosition)
+        assertEquals(Position(2, 2), state.tailPosition)
+    }
+
+    @Test
+    fun testMoveTailDiagonally2() {
+        val state = Day09.State(Position(2, 2), Position(1, 1), mutableListOf(Position(2, 2), Position(1, 1)))
+
+        state.playMove(Day09.Move(Day09.Direction.RIGHT, 1))
+
+        assertEquals(Position(3, 2), state.headPosition)
+        assertEquals(Position(2, 2), state.tailPosition)
     }
 
     @Test
