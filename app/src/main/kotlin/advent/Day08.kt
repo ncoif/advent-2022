3a691c0b41296aa71d097c2b6c18e667ade39717
ncoif <ncoif@gmx.com>
2022-12-16 08:56:28 +0000

    Day08 neighbour trees

--- app/src/main/kotlin/advent/Day08.kt
+++ app/src/main/kotlin/advent/Day08.kt
@@ -6,10 +6,27 @@ import java.net.URL
 class Day08(private val input: URL) {
 
     data class Tree(val height: Int, val visible: Boolean)
-    data class Grid(private val width: Int, private val height: Int, val trees: List<Tree>) {
+    data class Grid(val width: Int, val height: Int, val trees: List<Tree>) {
         fun position(w: Int, h: Int) : Tree {
             return trees[w + h * width]
         }
+
+        fun treesOnTop(w: Int, h: Int): List<Tree> {
+            return (0 until h).map { position(w, it) }
+        }
+
+        fun treesOnBottom(w: Int, h: Int): List<Tree> {
+            return (h + 1 until height).map { position(w, it) }
+        }
+
+        fun treesOnLeft(w: Int, h: Int): List<Tree> {
+            return (0 until w).map { position(it, w) }
+        }
+
+        fun treesOnRight(w: Int, h: Int): List<Tree> {
+            return (w + 1 until width).map { position(it, w) }
+        }
+
     }
 
     fun parseGrid(lines: List<String>) : Grid {
@@ -18,6 +35,10 @@ class Day08(private val input: URL) {
         }
 
         return Grid(lines[0].length, lines.size, trees)
+    }
+
+    fun updateVisibility(grid: Grid) {
+
     }
 
     fun solvePart1(): Long {
--- app/src/test/kotlin/advent/Day08Test.kt
+++ app/src/test/kotlin/advent/Day08Test.kt
@@ -3,6 +3,7 @@ package advent
 import org.junit.Test
 import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class Day08Test {
     private fun underTest() : Day08 {
@@ -35,6 +36,38 @@ internal class Day08Test {
 
         assertEquals(Day08.Tree(5, false), grid.position(1, 1))
         assertEquals(Day08.Tree(4, false), grid.position(3, 3))
+    }
+
+    @Test
+    fun testNeighbourTrees() {
+        val testInput = this.javaClass.getResource("/day08-test-input.txt")!!
+        val grid = underTest().parseGrid(File(testInput.toURI()).readLines())
+
+        val actualTop = grid.treesOnTop(1, 2)
+        assertEquals(2, actualTop.size)
+        assertEquals(Day08.Tree(0, false), actualTop[0])
+        assertEquals(Day08.Tree(5, false), actualTop[1])
+
+        val actualBottom = grid.treesOnBottom(1, 2)
+        assertEquals(2, actualBottom.size)
+        assertEquals(Day08.Tree(3, false), actualBottom[0])
+        assertEquals(Day08.Tree(5, false), actualBottom[1])
+
+        val actualLeft = grid.treesOnLeft(2, 2)
+        assertEquals(2, actualLeft.size)
+        assertEquals(Day08.Tree(6, false), actualLeft[0])
+        assertEquals(Day08.Tree(5, false), actualLeft[1])
+
+        val actualRight = grid.treesOnRight(2, 2)
+        assertEquals(2, actualRight.size)
+        assertEquals(Day08.Tree(3, false), actualRight[0])
+        assertEquals(Day08.Tree(2, false), actualRight[1])
+
+        val actualEdgeRight = grid.treesOnRight(4, 4)
+        assertTrue { actualEdgeRight.isEmpty() }
+
+        val actualEdgeBottom = grid.treesOnRight(4, 4)
+        assertTrue { actualEdgeBottom.isEmpty() }
     }
 
     @Test
