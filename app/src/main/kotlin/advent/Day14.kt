eed954cdf6453eb267afc22499ab2e238572410b
ncoif <ncoif@gmx.com>
2022-12-16 08:56:29 +0000

    Day14: make grid from rock lines

--- app/src/main/kotlin/advent/Day14.kt
+++ app/src/main/kotlin/advent/Day14.kt
@@ -5,9 +5,73 @@ import java.net.URL
 
 class Day14(private val input: URL) {
 
-    enum class Type { ROCK, AIR, SAND }
+    enum class Type(val printChar: Char) {
+        ROCK('#'),
+        AIR('.'),
+        SAND('o'),
+        START('+')
+    }
     data class Point(val x: Int, val y: Int)
     data class RockLine(val points: List<Point>)
+
+    data class Grid(
+        private val width: Int,
+        private val height: Int,
+        private val offsetX: Int,
+        private val offsetY: Int = 0) {
+
+        private val cells = mutableListOf<Type>()
+
+        init {
+            (0 until width * height).forEach{ _ -> cells.add(Type.AIR)}
+            position(500, 0, Type.START)
+        }
+
+        private fun x(x: Int) = x - offsetX
+        private fun y(y: Int) = y - offsetY
+
+        private fun position(x: Int, y: Int, type: Type) {
+            cells[x(x) + y(y) * width] = type
+        }
+
+        private fun position(x: Int, y: Int) = cells[x(x) + y(y) * width]
+
+        fun print() {
+            for (y in 0 until height) {
+                for (x in 0 until width) {
+                    print(cells[x + y * width].printChar)
+                }
+                println()
+            }
+            println()
+        }
+
+        fun fillRockLine(rockLine: RockLine) {
+            require(rockLine.points.size >= 2) { "Rock line doesn't have enough points" }
+            val iterator = rockLine.points.iterator()
+            var previous = iterator.next()
+            while (iterator.hasNext()) {
+                val current = iterator.next()
+                // trace line from previous to current
+                fillRockLine(previous, current)
+
+                // prepare next iteration
+                previous = current
+            }
+        }
+
+        private fun fillRockLine(start: Point, end: Point) {
+            if (start.x == end.x) {
+                //println("trace horizontal line from $start to $end")
+                (minOf(start.y, end.y) .. maxOf(start.y, end.y)).forEach { position(start.x, it, Type.ROCK)}
+            } else if(start.y == end.y) {
+                //println("trace vertical line from $start to $end")
+                (minOf(start.x, end.x) .. maxOf(start.x, end.x)).forEach { position(it, start.y, Type.ROCK)}
+            } else {
+                throw IllegalArgumentException("diagonal lines not supported")
+            }
+        }
+    }
 
     private fun parse(lines: List<String>) : List<RockLine> {
         return lines.map { line ->
@@ -20,9 +84,27 @@ class Day14(private val input: URL) {
         }
     }
 
+    private fun makeGrid(rockLines: List<RockLine>) : Grid {
+        val maxX = rockLines.flatMap { it.points }.maxOf { it.x }
+        val minX = rockLines.flatMap { it.points }.minOf { it.x }
+        val gridWidth = maxX - minX + 1
+
+        val maxY = rockLines.flatMap { it.points }.maxOf { it.y }
+        val minY = 0
+        val gridHeight = maxY - minY + 1
+
+        val grid = Grid(gridWidth, gridHeight, offsetX = minX, offsetY = 0)
+
+        rockLines.forEach { grid.fillRockLine(it) }
+
+        return grid
+    }
+
     fun solvePart1(): Int {
         val lines = File(input.toURI()).readLines()
-        println(parse(lines))
+        val rockLines = parse(lines)
+        val grid = makeGrid(rockLines)
+        grid.print()
         return 0
     }
 }
